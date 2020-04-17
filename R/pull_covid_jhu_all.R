62ca0f52bea848e337fb607c01c87c6c8945ddab
Slushmier <aschless@gwu.edu>
2020-04-17 11:56:33 -0400

    Fixed code b/c of file structure change

--- R/pull_covid_jhu_all.R
+++ R/pull_covid_jhu_all.R
@@ -6,33 +6,33 @@ library(tidyr)
 request <- GET("https://api.github.com/repos/CSSEGISandData/COVID-19/git/trees/master?recursive=1")
 stop_for_status(request)
 files <- unlist(lapply(content(request)$tree, "[", "path"))
-drs <- grepl(files, pattern = "csse_covid_19_data/csse_covid_19_daily_report")
+drs <- grepl(files, pattern = "csse_covid_19_data/csse_covid_19_daily_reports/")
 daily_reports <- files[drs]
 
 x <- length(daily_reports) -1
 
-for(i in seq(from = 3, x, 1)){
+for(i in seq(from = 2, x, 1)){
   url <- paste0("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/",
                 daily_reports[i])
   daily <- read.csv(url)
   
-  if(i >= 42 & i < 63){
+  if(i >= 41 & i < 62){
     daily <- daily %>% select(-Latitude, -Longitude)
   }
   
   #if(i < 13 | (i >= 54 & i < 63)){
-  if(i < 63){
+  if(i < 62){
     colnames(daily) <- c("Province_State", "Country_Region", "Last_Update",
                          "Confirmed", "Deaths", "Recovered")  
   }
   
-  if(i < 63){
+  if(i < 62){
     daily <- daily %>% 
       replace_na(list(Confirmed = 0, Deaths = 0, Recovered = 0)) %>% 
       mutate(Active = Confirmed - Deaths - Recovered)
   }
   
-  if(i >= 63){
+  if(i >= 62){
     daily <- daily[-1]
     daily <- daily %>% select(-Admin2, -Lat, -Long_,
                               -Combined_Key) %>% 
